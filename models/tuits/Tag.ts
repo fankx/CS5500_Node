870cec6cd04957a08227424567d9f12ed1929820
Frankie Luo <luo.jingy@northeastern.edu>
2022-02-03 20:53:15 -0500

    finished A1.

---
+++ models/tuits/Tag.ts
@@ -0,0 +1,6 @@
+import Tuit from './Tuit';
+
+export default class Tag {
+  private tag: string = '';
+  private tuit: Tuit | null = null;
+}
--- server.ts
+++ server.ts
@@ -1,8 +1,25 @@
-import express from 'express';
+/**
+ * @file Server file
+ */
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+mongoose.connect(
+  'mongodb+srv://test:test@frankiemedia.rfsor.mongodb.net/tuit?retryWrites=true&w=majority'
+);
+import bodyParser from 'body-parser';
+import UserController from './controllers/UserController';
+import TuitController from './controllers/TuitController';
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/hello', (req, res) =>
-    res.send('Hello World!'));
+app.get('/', (req: Request, res: Response) => res.send('Welcome!'));
+
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+  res.send(req.params.a + req.params.b)
+);
+
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
