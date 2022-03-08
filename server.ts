852ef0649562261105c9508b39569d8054c0fd97
Frankie Luo <luo.jingy@northeastern.edu>
2022-03-08 17:16:32 -0500

    modified env variable.

--- server.ts
+++ server.ts
@@ -6,25 +6,25 @@
  *     <li>tuits</li>
  *     <li>likes</li>
  * </ul>
- * 
+ *
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
-import express, {Request, Response} from 'express';
-import CourseController from "./controllers/CourseController";
-import UserController from "./controllers/UserController";
-import TuitController from "./controllers/TuitController";
-import LikeController from "./controllers/LikeController";
-import mongoose from "mongoose";
-var cors = require('cors')
+import express, { Request, Response } from 'express';
+import CourseController from './controllers/CourseController';
+import UserController from './controllers/UserController';
+import TuitController from './controllers/TuitController';
+import LikeController from './controllers/LikeController';
+import mongoose from 'mongoose';
+var cors = require('cors');
 
 // build the connection string
-const PROTOCOL = "mongodb+srv";
+const PROTOCOL = 'mongodb+srv';
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
+const HOST = 'frankiemedia.rfsor.mongodb.net';
+const DB_NAME = 'tuit';
+const DB_QUERY = 'retryWrites=true&w=majority';
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
 mongoose.connect(connectionString);
@@ -33,11 +33,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
+app.get('/', (req: Request, res: Response) => res.send('Welcome!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
+  res.send(req.params.a + req.params.b)
+);
 
 // create RESTful Web service API
 const courseController = new CourseController(app);
